be41ddf86060adb49a2db1606afced76efe3af4b
SpicyTaterTot00 <mcfarland_jessica1@columbusstate.edu>
2021-11-15 16:45:13 +0000

    reactstrap

--- my_react_app/src/App.js
+++ my_react_app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import{ Card } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    let cname = "lead";
+
+    return(
+      // <div>
+      //   <Jumbotron>
+      //     <h1 classname= "display 3"> My First React App</h1>
+      //     <p classname={cname}> My first attempt at it</p>
+      //     <br classname={my-2}/>
+      //     <p>Playing with reactstrap</p>
+      //     <p classname={cname}></p>
+      //   </Jumbotron>
+      // </div>
+    )
+  }
 }
 
 export default App;
